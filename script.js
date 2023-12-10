37bdf99a7277c12794e63f51c73c5e02cae3c93d
adeleke <babatundeola876@gmail.com>
2023-12-09 16:34:46 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const inputfield = document.getElementById("inputSpace");
+const calculator = document.getElementsByClassName("calculator");
+
+function display(num){
+    inputfield.value += num;
+}
+function clean(){
+    inputfield.value = '';
+}
+
+function del(){
+    inputfield.value = inputfield.value.slice(0,-1);
+}
+
+
+
+function calculate(){
+   inputfield.value= eval(inputfield.value);
+  
+}
+
+function plus() {
+    
+}
+
+// save data on the Browser
+
+
